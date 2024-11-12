63889117d15cbb9e3834ab6ba0fbeb09f4d736c3
Sumit Jadhav <summit.jadhav@gmail.com>
2024-11-12 12:55:06 +0530

    Update social media links

--- src/components/FooterWrapper/index.tsx
+++ src/components/FooterWrapper/index.tsx
@@ -1,5 +1,6 @@
 import { Footer } from 'flowbite-react'
-import { BsFacebook, BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
+import { BsGithub, BsLinkedin, BsEnvelopeAtFill } from 'react-icons/bs'
+
 
 const FooterWrapper = () => <Footer container className="rounded-none">
     <div className="container max-w-5xl mx-auto">
@@ -7,10 +8,9 @@ const FooterWrapper = () => <Footer container className="rounded-none">
         <div className="border-0 w-full px-4 py-6 sm:flex sm:items-center sm:justify-between">
             <Footer.Copyright href="#" by="Sumit Suresh Jadhav™" year={2024} />
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-                <Footer.Icon href="#" icon={BsFacebook} />
-                <Footer.Icon href="#" icon={BsInstagram} />
-                <Footer.Icon href="#" icon={BsGithub} />
-                <Footer.Icon href="#" icon={BsLinkedin} />
+                <Footer.Icon href="mailto:summit.jadhav@gmail.com" icon={BsEnvelopeAtFill} />
+                <Footer.Icon href="https://github.com/sumitsj" target="_blank" icon={BsGithub} />
+                <Footer.Icon href="https://www.linkedin.com/in/sumitsjadhav/" target="_blank" icon={BsLinkedin} />
             </div>
         </div>
     </div>
